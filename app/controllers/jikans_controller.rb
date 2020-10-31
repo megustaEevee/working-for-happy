8543738e992d15da0c6881d6efa1bd404511ba2b
megustaEevee <kotori_cat@yahoo.co.jp>
2020-10-31 15:02:34 +0900

    出社機能の実装

--- app/controllers/jikans_controller.rb
+++ app/controllers/jikans_controller.rb
@@ -1,10 +1,28 @@
 class JikansController < ApplicationController
   def index
     @user = User.all
+    @jikan = Jikan.new
+    @time = Time.now
   end
 
   def show
-    
+    @user = User.all
+    @time = Time.now
+  end
+
+  def create
+    @jikan = Jikan.new(jikan_params)
+    if @jikan.save
+      redirect_to @jikan
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def jikan_params
+    params.require(:jikan).permit(:start_time).merge(user_id: current_user.id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: "jikans#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :jikans, only: [:index, :show]
+  resources :jikans, only: [:index, :show, :create]
 end
