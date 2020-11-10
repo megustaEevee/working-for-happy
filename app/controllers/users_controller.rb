c8473e4c6a8082e8082ffc0cd8e20b836fcdb162
megustaEevee <kotori_cat@yahoo.co.jp>
2020-11-10 15:34:11 +0900

    賃金計算機能の修正
    超過勤務と夜間勤務、重複分について、計算機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,21 +2,13 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = User.find(params[:id])
-    @wages = Wage.order(id: :DESC).where(user_id: @user.id)
-    @work = Work.order(id: :DESC).find_by(user_id: @user.id)
-    @comments = Comment.order(id: :DESC).where(user_id: @user.id)
-
-    @last_day = @work.created_at.strftime('%Y/%m/%d').to_s if @work
-
     @time = Time.now
-    day = if @time.day < 10
-            "0#{@time.day}"
-          else
-            @time.day.to_s
-          end
-    @today = "#{@time.year}/#{@time.month}/#{day}"
 
+    @user = User.find(params[:id])
+    @work = Work.order(id: :DESC).find_by(user_id: @user.id)
     @wage = @user.wages.find_by(work_id: @work.id) if @user.wages != []
+
+    @wages = Wage.order(id: :DESC).where(user_id: @user.id)
+    @comments = Comment.order(id: :DESC).where(user_id: @user.id)
   end
 end
